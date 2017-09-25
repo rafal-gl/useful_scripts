17707851e7512732be61cdc58f9885d74a102096
rafal-gl <32041012+rafal-gl@users.noreply.github.com>
2017-09-25 15:49:25 +0200

    Uogólnienie do dowolnego pakietu

--- stworz_pakiet.R
+++ stworz_pakiet.R
@@ -1,5 +1,5 @@
 #####################################################################
-# Skrypt do budowy kolejnych wersji pakietu getinCRM
+# Skrypt do budowy kolejnych wersji pakietu
 # Autor: Rafał Głąbski
 #####################################################################
 
@@ -7,10 +7,20 @@ library(devtools)
 library(roxygen2)
 library(stringr)
 library(dplyr)
+
+package_name <- "getinCRM"
+
 old_wd <- getwd()
-setwd("P:/Departament Kampanii/ZESPOL_ANALIZ/R/pakiety/budowa_pakietu")
+# ścieżka do folderu gdzie powstaną pliki pakietowe
+# musi zawierać podfolder z wszystkimi funkcjami do pakietu o nazwie
+# nazwapakietu_funkcje
+package_dir <- "P:/Departament Kampanii/ZESPOL_ANALIZ/R/pakiety/budowa_pakietu"
+# ścieżka z poprzednimi wersjami pakietu
+old_versions_dir <- "P:/Departament Kampanii/ZESPOL_ANALIZ/R/pakiety"
+
+setwd(package_dir)
 options(devtools.desc.author="'First Last <first.last@example.com> [aut, cre]'")
-unlink('getinCRM', recursive = TRUE)
+unlink(package_name, recursive = TRUE)
 
 ### Numer następnej wersji ###
 # Za change_ind wybierz liczbę od 1 do 4, im mniejsza tym ważniejsza zmiana 
@@ -21,7 +31,7 @@ unlink('getinCRM', recursive = TRUE)
 change_ind = 4
 change_levels = c("major", "minor", "build", "revision")
 
-vers = grep(".tar.gz", dir("P:/Departament Kampanii/ZESPOL_ANALIZ/R/pakiety"), 
+vers = grep(".tar.gz", dir(old_versions_dir), 
             value = TRUE)
 vers = unlist(str_extract_all(vers, "\\d(\\.(\\d)+)*"))
 last_ver = vers[rank(-as.numeric(gsub('\\.', '', vers)))[1]]
@@ -35,34 +45,37 @@ if(change_ind < length(change_levels)) {
 } 
 new_ver = paste(new_ver_num, collapse = ".")
 
-create(path = "getinCRM",
-       description = list(Title = "Modelowanie w ZM DSG",
+create(path = "package_name",
+       description = list(Title = "Title",
                           Version = new_ver,
-                          Description = "Kilka przydatnych funkcji w modelowaniu predykcyjnym w Zespole Analiz i Modelowania w GNB",
+                          Description = "Description",
                           License = "Internal use only",
                           "Authors@R" = "c(person(given = \"Rafal\", family = \"Glabski\", email = \"rafal.glabski@gnb.pl\", role = c(\"aut\", \"cre\")))", 
-                          Author = "Rafal Glabski, Sebastian Gargas, Karol Urban",
-                          Maintainer = "Rafal Glabski <rafal.glabski@gnb.pl>",
-                          #Suggests = "ROracle",
+                          Author = "Rafal Glabski",
+                          Maintainer = "Rafal Glabski <rafal.glabski@mail.pl>",
+                          # Hadley mówi, żeby wypisywać tylko Imports, a zamiast Depends
+                          # używać tylko package::function albo requireNamespace,
+                          # ale tak jest wygodniej :P
                           Depends = "broom, C50, DBI, dplyr, geosphere, ggmap, ggplot2, methods, partykit, purrr, reshape2, rgeos, rJava, RJDBC, rjson, ROCR, RODBC, sp, stringr, tcltk, xtable",
                           Imports = "broom, C50, DBI, dplyr, geosphere, ggmap, ggplot2, methods, partykit, purrr, reshape2, rgeos, rJava, RJDBC, rjson, ROCR, RODBC, sp, stringr, tcltk, xtable",
                           Encoding = "UTF-8"))
 
-file.copy(list.files("getinCRM_funkcje", full.names = TRUE),
-          to = "getinCRM/R")
-setwd("./getinCRM")
+file.copy(list.files(paste0(package_name, "_funkcje"), 
+                     full.names = TRUE),
+          to = paste0(package_name, "/R"))
+setwd(paste0("./", package_name))
 document()
 setwd("..")
-install("getinCRM")
-library(getinCRM)
-# check("getinCRM")
+install(package_name)
+library(...)
+# check(package_name)
 
-build(pkg = 'getinCRM', path = "P:/Departament Kampanii/ZESPOL_ANALIZ/R/pakiety")
+build(pkg = package_name, path = old_versions_dir)
 
 setwd(old_wd)
 
 # dokumentacja
-# pack <- "getinCRM"
+# pack <- package_name
 # path <- find.package(pack)
 # system(paste(shQuote(file.path(R.home("bin"), "R")),
 #              "CMD", "Rd2pdf", shQuote(path)))
